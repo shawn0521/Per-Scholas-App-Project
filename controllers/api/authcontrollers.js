ec6f85e4006e8a1c915643cf65ec72b2fd0f00d3
shawn0521 <110927715+shawn0521@users.noreply.github.com>
2022-11-22 09:48:31 -0500

    Add files via upload

---
+++ controllers/api/authcontrollers.js
@@ -0,0 +1,34 @@
+const user      = require('../../models/user')
+const bcrypt    = require('bcryptjs')
+const jwt       = require('jsonwebtoken')
+
+const register = (req,res,next) => {
+    bcrypt.hash(req.body.password, 10,function(err,hashedPass){
+        if(err) {
+            res.json({
+                error: err
+            })
+        }
+    })
+}
+
+let. user = new user ({
+    name:   req.body.name,
+    email:  req.body.email,
+    password: hashedPass
+})
+user.save()
+.then(user => {
+    res.json({
+        message: 'User Added Successfully!'
+    })
+})
+.catch(error => {
+    res.json({
+        message: 'An Error Occured!'
+    })
+})
+
+module.exports = {
+    register
+}
